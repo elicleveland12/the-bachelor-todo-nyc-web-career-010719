b6302e553fb1810c2772113ae8a83f97dd3011fe
Eli Cleveland <elicleveland12@gmail.com>
2019-01-04 19:18:48 +0000

    Automatically backed up by Learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -1,17 +1,40 @@
+require 'pry'
+
 def get_first_name_of_season_winner(data, season)
-  # code here
+  data[season].each do |info|
+    if info["status"] == "Winner"
+      return info["name"].split(" ")[0]
+    end
+  end
 end
 
 def get_contestant_name(data, occupation)
-  # code here
+  data.each do |season, info|
+    info.each do |value|  
+      if value["occupation"] == occupation
+        return value["name"]
+      end
+    end
+  end 
 end
 
 def count_contestants_by_hometown(data, hometown)
-  # code here
+  counter = 0
+  data.each do |season, info|
+    info.each do |value|
+      if value["hometown"] == hometown
+        counter += 1 
+      end
+    end
+  end
+  return counter
 end
 
 def get_occupation(data, hometown)
-  # code here
+  data.each do |season, info|
+    info.each do |value|
+      if value["hometown"] == hometown
+        return 
 end
 
 def get_average_age_for_season(data, season)
